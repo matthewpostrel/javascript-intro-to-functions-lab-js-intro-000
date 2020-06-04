8459cfe63a105b62481b6fae32fd407b6b13c2f2
suk jayhay <mattpostrel7@gmail.com>
2020-06-04 23:43:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,16 +15,11 @@ console.log(string.toLowerCase())
 }
 
 function sayHiToGrandma(string) {
-
-      if (string === string.toLowerCase()){
-        var lowerCase = string.toLowerCase();
-        return ('I can\'t hear you!');
-      }
-      else if (string === string.toUpperCase()){
-        var upperCase = 'YES INDEED!';
-        return ('YES INDEED!');
-        }
-        else{
-          var mixedCase = 'I love you, too.';
-          return(mixedCase);
-        }
+  if (string === string.toLowerCase()) {
+      return "I can't hear you!";
+  }
+  else if (string === string.toUpperCase()) {
+      return "YES INDEED!";
+  } else if ('I love you, Grandma.') {
+    return 'I love you, too.'
+  }
