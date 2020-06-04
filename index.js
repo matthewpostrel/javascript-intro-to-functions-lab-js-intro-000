08c743bd906c44e86c809ec52b71ef93ca8522c2
suk jayhay <mattpostrel7@gmail.com>
2020-06-04 23:36:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,7 +15,11 @@ console.log(string.toLowerCase())
 }
 
 function sayHiToGrandma(string) {
-if(string === string.toLowercase()) return "I can't hear you!";
-else if (string === "I love you grandma.") return "I love you, too.";
-else if (string === string.toUppercase()) return "YES INDEED!"
+  function sayHiToGrandma(string) {
+  if (string.toLowerCase() === string) {
+  return “I can’t hear you!” ;
+  } if (string.toUpperCase() === string ) {
+  return “YES INDEED!”
+  } if (string === `I love you, Grandma.` ) {
+  return “I love you, too.” }
 }
